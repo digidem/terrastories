43aef4134f4bd27f7942ffb53bfcada4d231e45e
luandro <luandro@digital-democracy.org>
2025-11-05 23:07:15 -0300

    Restore audio MIME support for Active Storage

--- rails/spec/models/media_spec.rb
+++ rails/spec/models/media_spec.rb
@@ -19,8 +19,10 @@ RSpec.describe Media, type: :model do
     it "validates audio types" do
       is_expected.to validate_content_type_of(:media).allowing(
         'audio/mpeg',
+        'audio/wav',
         'audio/x-wav',
         'audio/mp4',
+        'audio/m4a',
         'audio/x-m4a',
         'audio/x-aac',
         'audio/x-flac',
