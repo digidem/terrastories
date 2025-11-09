e6ec4e393c5c0e8f68672c4239e41c3f4dc2f3db
luandro <luandro@digital-democracy.org>
2025-11-09 07:34:41 -0300

    Support glibc Ruby base images for local tests

--- rails/config/environments/test.rb
+++ rails/config/environments/test.rb
@@ -52,6 +52,11 @@ Rails.application.configure do
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
 
+  # Allow default host used by Rails test helpers.
+  config.hosts.clear
+
+  config.web_console.enabled = false if config.respond_to?(:web_console)
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
