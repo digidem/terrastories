edcb226ee7f09bd39b7476e8ba852596571905ab
luandro <luandro@digital-democracy.org>
2025-11-23 14:53:24 -0300

    Move expose Mapbox toggle and disable when private

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_10_210545) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_20_170000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -175,6 +175,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_210545) do
     t.bigint "community_id", null: false
     t.text "protomaps_api_key"
     t.text "protomaps_basemap_style"
+    t.boolean "expose_mapbox_credentials", default: false, null: false
   end
 
   create_table "users", force: :cascade do |t|
